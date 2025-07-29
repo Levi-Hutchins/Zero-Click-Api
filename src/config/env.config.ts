72cc58fee1ee61e1fce75902f7aa5df6dbca1c0a
Levi Hutchins <levihutchins0@gmail.com>
2025-07-29 16:07:30 +1000

    fix yarn and npm conflict

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import { defineEnv, validateEnv } from 'node-env-schema';
 
 const envFile = process.env.NODE_ENV === 'production' ? 'production.env' : 'development.env';
+
 dotenv.config({ path: envFile });
 
 const schema = defineEnv({
@@ -11,6 +12,11 @@ const schema = defineEnv({
   ENABLE_FEATURE: { type: 'boolean', default: false },
   TEST: { type: 'boolean', default: false },
   ALLOWED_ORIGINS: { type: 'string', default: 'localhost' },
+  MONGODB_CONNECTION_STRING: { type: 'string', default: '' },
+  MONGODB_DATABASE: { type: 'string', default: '' },
+  SCAN_RESULTS_COLLECTION: { type: 'string', default: '' },
+  SCAN_SUBMISSIONS_COLLECTION: { type: 'string', default: '' },
+
 });
 
 export const env = validateEnv(schema);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import app from './app';
+import { logger } from './utils/logger';
 import { env } from './config/env.config';
 
 const PORT = env.PORT;
 const TEST = env.TEST;
 app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
   console.log(`Server is running on port ${PORT}`);
   console.log(`Test mode is ${TEST}`);
-  console.log('Raw env:', process.env.TEST); // should show 'true'
+  console.log('Raw env:', process.env.MONGODB_CONNECTION_STRING); // should show 'true'
 });
